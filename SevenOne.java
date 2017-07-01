0ed74153d9cee9c5c9ee3860bb88590f5134413b
ITSOK\anonm <dev1ender@gmail.com>
2017-07-02 00:15:00 +0530

    last

--- SevenOne.java
+++ SevenOne.java
@@ -1,7 +1,7 @@
 public class SevenOne{
 	public static void main(String []args){
 	int a =123;
-	String string = String.valueOf(a);
+	String string = String.valueOf(a); //value of method is used to convert int to string it fetch the value only 
 	System.out.print(string);
 	}
 }
